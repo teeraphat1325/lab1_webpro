499ef6a5f23d023224178ab85f626b0260b7e95c
Teeraphat Prannok <66160068@go.buu.ac.th>
2024-12-01 22:48:31 +0700

    Finish counter3 with vuejs component

---
+++ counter-project/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+  const counter = ref(0);
+  function increase() {
+    counter.value++;
+    console.log(counter.value)
+  }
+</script>
+
+<template>
+  <button @click="increase">Count is: {{ counter }}</button>
+  <button @click="counter=0">Reset</button>
+</template>
+
+<style scoped>
+
+</style>
